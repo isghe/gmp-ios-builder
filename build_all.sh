a21b163010611325a7486de990d4e6cba68dc632
Isidoro Ghezzi <isidoro.ghezzi@icloud.com>
2016-10-10 15:24:17 +0200

    build_all.sh

---
+++ build_all.sh
@@ -0,0 +1,21 @@
+# Clean:
+
+./buildIosGmpFramework -c
+
+# Extract:
+
+./buildIosGmpFramework -e
+
+# Config:
+
+./buildIosGmpFramework -g
+
+# Build all (this can take a while):
+
+./buildIosGmpFramework -b
+
+# Build framework:
+
+./buildIosGmpFramework -f
+
+
